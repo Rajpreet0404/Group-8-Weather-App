70b66dfd710cf92a9af3ec4d743291e0501016fa
Sulaiman <sulaiman.arif@outlook.com>
2025-03-18 23:49:37 +0000

    Updated homepage

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="background">
       <div className="container">
+
         <div className="box location">
           <div className="weatherimage">
             <img src="/locationimage.png" alt="Background" />
@@ -14,6 +15,26 @@ function App() {
             <h1>Current Location</h1>
           </div>
         </div>
+
+        <div className="box weather">
+          <div className="currentweather">
+            <h1>19°</h1>
+          </div>
+          <div className="weathericon">
+            <img src="/currentweather.png" alt="Current Weather"/>
+          </div>
+          <div className="windspeed">
+            <h1>Wind Speed: 6 mph</h1>
+          </div>
+          <div className="feelslike">
+            <h1>Feels like 19°</h1>
+          </div>
+        </div>
+
+        <div className="box temperature">
+          
+        </div>
+
       </div>
     </div>
   );
