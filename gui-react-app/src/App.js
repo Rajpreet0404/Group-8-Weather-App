e8dfa0eba8d2acd62f882b857f3919f2e7e4c844
Sulaiman <sulaiman.arif@outlook.com>
2025-03-18 20:11:37 +0000

    home page

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -4,13 +4,15 @@ import "./style.css"; // Import the CSS file
 
 function App() {
   return (
-    <div className="container">
-      <div className="box location">
-        <div className="weatherimage">
-          <img src="/locationimage.png" alt="Background" width="300" />
-        </div>
-        <div className="locationtext">
-          <h1>Current Location</h1>
+    <div className="background">
+      <div className="container">
+        <div className="box location">
+          <div className="weatherimage">
+            <img src="/locationimage.png" alt="Background" />
+          </div>
+          <div className="locationtext">
+            <h1>Current Location</h1>
+          </div>
         </div>
       </div>
     </div>
