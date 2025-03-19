086f701edb585ea19a0d42affdd1c479c7a95cca
Rajpreet0404 <r.athwal@se23.qmul.ac.uk>
2025-03-19 09:23:27 +0000

    modify App.js and style.css for layout improvements

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -5,39 +5,35 @@ import "./style.css"; // Import the CSS file
 
 function App() {
   return (
-    <div className="background">
-      <div className="container">
-
-        <div className="box location">
-          <div className="weatherimage">
-            <img src="/locationimage.png" alt="Background" />
-          </div>
-          <div className="locationtext">
-            <h1>Current Location</h1>
-          </div>
+    <body>
+      <div className="box location">
+        <div className="weatherimage">
+          <img src="/locationimage.png" alt="Background" />
         </div>
-
-        <div className="box weather">
-          <div className="currentweather">
-            <h1>19°</h1>
-          </div>
-          <div className="weathericon">
-            <img src="/currentweather.png" alt="Current Weather"/>
-          </div>
-          <div className="windspeed">
-            <h1>Wind Speed: 6 mph</h1>
-          </div>
-          <div className="feelslike">
-            <h1>Feels like 19°</h1>
-          </div>
+        <div className="locationtext">
+          <h1>Current Location</h1>
         </div>
-
-        <div className="box temperature">
-          
-        </div>
-
       </div>
-    </div>
+
+      <div className="box weather">
+        <div className="currentweather">
+          <h1>19°</h1>
+        </div>
+        <div className="weathericon">
+          <img src="/currentweather.png" alt="Current Weather"/>
+        </div>
+        <div className="windspeed">
+          <h1>Wind Speed: 6 mph</h1>
+        </div>
+        <div className="feelslike">
+          <h1>Feels like 19°</h1>
+        </div>
+      </div>
+
+      <div className="box temperature">
+        
+      </div>
+    </body>
   );
 }
 
