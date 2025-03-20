512414a79a07c91ee278f226ef23102363d51574
Sulaiman <sulaiman.arif@outlook.com>
2025-03-20 01:06:12 +0000

    start of settings page with title and sidebar icon

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -1,40 +1,25 @@
 // App.js
 import React from "react";
-import "./reset.css"; // Import the CSS file
+import "./reset.css"
 import "./style.css"; // Import the CSS file
 
 function App() {
   return (
-    <body>
-      {/* Location flex box */}
-      <section className="locationBox">
-        <div className="weatherimage">
-          <img src="/locationimage.png" alt="Background" />
-        </div>
-        <div className="locationtext">
-          <h1>Current Location</h1>
-        </div>
-      </section>
-      {/* Weather flex box */}
-      <section className="weatherBox">
-        <div className="currentweather">
-          <h1>19°</h1>
-        </div>
-        <div className="weathericon">
-          <img src="/currentweather.png" alt="Current Weather"/>
-        </div>
-        <div className="windspeed">
-          <h1>Wind Speed: 6 mph</h1>
-        </div>
-        <div className="feelslike">
-          <h1>Feels like 19°</h1>
-        </div>
-      </section> 
+    <div className="container">
 
-      <div className="box temperature">
-        
-      </div>
-    </body>
+      <header className="box sidebar">
+        <div className="sidebarimage">
+          <img src="/sidebaricon.png" alt="sidebar" />
+        </div>
+      </header>
+
+      <header className="box title">
+        <div className="settingstext">
+          <h1>Settings</h1>
+        </div>         
+      </header>
+      
+    </div>
   );
 }
 
