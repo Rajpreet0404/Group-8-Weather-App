15a3c0654916d95be56f52fb3a63fac96e60d99e
Sulaiman <sulaiman.arif@outlook.com>
2025-03-20 05:44:54 +0000

    Update to the settings page - still needs editing

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -7,17 +7,105 @@ function App() {
   return (
     <div className="container">
 
-      <header className="box sidebar">
+      <nav className="box sidebar">
         <div className="sidebarimage">
           <img src="/sidebaricon.png" alt="sidebar" />
         </div>
-      </header>
+      </nav>
 
       <header className="box title">
         <div className="settingstext">
           <h1>Settings</h1>
         </div>         
       </header>
+
+      <section className="box appearance">
+        <div className="appearancetitle">
+          <h1>Appearance</h1>
+        </div>
+        <article className="settings darkmode">
+          <h1>Dark Mode</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Dynamic Background</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Font Size</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Temperature Unit</h1>
+          <h1>test</h1>      
+        </article>
+      </section>
+
+      <section className="box appearance">
+        <div className="appearancetitle">
+          <h1>Location Settings</h1>
+        </div>
+        <article className="settings darkmode">
+          <h1>Dark Mode</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Dynamic Background</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Font Size</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Temperature Unit</h1>
+          <h1>test</h1>      
+        </article>
+      </section>
+
+      <section className="box appearance">
+        <div className="appearancetitle">
+          <h1>Notification Settings</h1>
+        </div>
+        <article className="settings darkmode">
+          <h1>Dark Mode</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Dynamic Background</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Font Size</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Temperature Unit</h1>
+          <h1>test</h1>      
+        </article>
+      </section>
+
+      <section className="box appearance">
+        <div className="appearancetitle">
+          <h1>About and Support</h1>
+        </div>
+        <article className="settings darkmode">
+          <h1>Dark Mode</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Dynamic Background</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Font Size</h1>
+          <h1>test</h1>      
+        </article>
+        <article className="settings dynamic">
+          <h1>Temperature Unit</h1>
+          <h1>test</h1>      
+        </article>
+      </section>
       
     </div>
   );
