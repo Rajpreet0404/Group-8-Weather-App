1eb115223da988c2b9ed7cc691e038cd95cc8314
Sulaiman <sulaiman.arif@outlook.com>
2025-03-25 16:03:43 +0000

    retractable sidebar

--- gui-react-app/src/App.js
+++ gui-react-app/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Settings from "./pages/settings";
 import Home from "./pages/home"
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Routes>
         <Route path="/settings" element={<Settings />} />
         <Route path="/" element={<Home />} />
---
+++ gui-react-app/src/components/sidebar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  const [closedmenu, setclosemenu] = useState(true);
+
+  const handleclosemenu = () => {
+    setclosemenu(!closedmenu);
+  };
+
+  return (
+    <>
+      {/* When sidebar is closed, show only the open menu button */}
+      {closedmenu && (
+        <div className="openmenu" onClick={handleclosemenu}>
+          <img src="../../sidebaricon.png" className="openbutton" alt="Open Menu" />
+        </div>
+      )}
+
+      {/* When sidebar is open, show full sidebar */}
+      <div className={closedmenu ? "sidebarinactive" : "sidebar"}>
+        <div className="closemenu" onClick={handleclosemenu}>
+          <img src="../../closemenu.png" className="closebutton" alt="Close" />
+        </div>
+        <div className="greeting">
+          <h1>Hello,</h1>
+          <h2>John Doe</h2>
+        </div>
+        <div className="items">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Profile</a></li>
+            <li><a href="/">My Sports</a></li>
+            <li><a href="/">Settings</a></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
